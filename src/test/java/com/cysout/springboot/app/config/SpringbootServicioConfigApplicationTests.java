934b810e85e05e1ff07e6ce462b5512b4c5a2620
fernando.ramirez <fernando.ramirez@infotec.mx>
2021-05-26 18:46:31 -0500

    Agrego el yml para items

---
+++ src/test/java/com/cysout/springboot/app/config/SpringbootServicioConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cysout.springboot.app.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicioConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
